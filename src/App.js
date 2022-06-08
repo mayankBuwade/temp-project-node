b4e6e3391d96c9bc4fa906d79bc39884dba70c42
mayank_buwade <mayank.buade@gmail.com>
2022-06-08 15:53:49 +0530

    v1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Blogs from "./components/blogs/blogs";
+import ContactUs from "./components/contactUs/contactUs";
+import Header from "./components/header/header";
+import Homepage from "./components/homepage/homepage";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Blogs />
+    </>
+  );
+};
+
+export default App;
